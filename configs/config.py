33e82be55ca4d35e06d58a18c24638439553f278
Fieldry <19241078@buaa.edu.cn>
2024-11-11 20:25:57 +0800

    fix: update config example

--- configs/config.py
+++ configs/config.py
@@ -1,28 +1,33 @@
-ml_param = {
-    "model": "CatBoost",
-    "dataset": "tjh",
-    "task": "outcome",  # "outcome" or "los"
-    "max_depth": 5,
-    "n_estimators": 100,
-    "learning_rate": 0.1,
-    "batch_size": 81920,
-    "main_metric": "auprc",
-}
+ml_example_params = [
+    {
+        "model": "CatBoost",
+        "dataset": "tjh",
+        "task": "outcome",  # "outcome" or "los"
+        "seed": 0,
+        "max_depth": 5,
+        "n_estimators": 100,
+        "learning_rate": 0.1,
+        "batch_size": 81920,
+        "main_metric": "auprc",
+    },
+]
 
-dl_example_param = {
-    "model": "RNN",
-    "dataset": "tjh",
-    "task": "outcome", # "outcome", "los" or "multitask"
-    "seed": 0,
-    "epochs": 100,
-    "patience": 10,
-    "batch_size": 64,
-    "learning_rate": 0.001,
-    "main_metric": "auprc",
-    "demo_dim": 2,
-    "lab_dim": 73,
-    "hidden_dim": 32,
-    "output_dim": 1,
-    "accelerator": "gpu", # "cpu" or "gpu"
-    "device": 0, # gpu device id if accelerator is "gpu"
-},
+dl_example_params = [
+    {
+        "model": "RNN",
+        "dataset": "tjh",
+        "task": "outcome", # "outcome", "los" or "multitask"
+        "seed": 0,
+        "epochs": 100,
+        "patience": 10,
+        "batch_size": 64,
+        "learning_rate": 0.001,
+        "main_metric": "auprc",
+        "demo_dim": 2,
+        "lab_dim": 73,
+        "hidden_dim": 32,
+        "output_dim": 1,
+        "accelerator": "gpu", # "cpu" or "gpu"
+        "devices": [0], # gpu device id if accelerator is "gpu"
+    },
+]
